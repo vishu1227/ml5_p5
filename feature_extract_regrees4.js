a396396290359343397a7597e8d33f5093fb0c74
Bharat Bhusan <bh27122000@gmail.com>
2021-03-16 12:08:18 +0530

    modified

---
+++ feature_extract_regrees4.js
@@ -0,0 +1,80 @@
+let mobileNet;
+let predictor;
+let video;
+let value=0;
+let slider;
+let addButton;
+let trainButton;
+
+function modelReady()
+{
+    console.log('Model is ready!');
+}
+
+function videoReady()
+{
+    console.log('Video is ready!');
+}
+
+function whileTraining(loss)
+{
+    if(loss==null)
+    {
+        console.log('Training Complete');
+        predictor.predict(gotResults);    
+    }
+    else
+    {
+        console.log(loss);
+    }
+}
+
+function gotResults(err,res)
+{
+    if(err)
+    {
+        console.log(err);
+    }
+    else
+    {
+        value=res.value;
+        predictor.predict(gotResults);
+    }
+}
+
+function setup()
+{
+    createCanvas(320,270);
+    video=createCapture(VIDEO);
+
+    video.hide();
+    background(0);
+
+    mobileNet=ml5.featureExtractor('MobileNet',modelReady);
+    predictor=mobileNet.regression(video,videoReady);
+
+    slider=createSlider(0,1,0.5,0.01);
+
+    addButton=createButton('add example image');
+    addButton.mousePressed(()=>{
+        predictor.addImage(slider.value());
+    })
+
+    trainButton=createButton('train');
+    trainButton.mousePressed(()=>{
+        predictor.train(whileTraining);
+    })
+}
+
+function draw()
+{
+    background(0);
+    image(video,0,0,320,240);
+    rectMode(CENTER);
+    fill(255,0,200);
+    rect(value*width,height/2,50,50);
+
+    fill(255);
+    textSize(16);
+    text(value,10,height-10);
+}
